f775e80d97c4fda0015d2417f6b9c2c9c34f4abb
María Aranda Iglesias <m.aranda.iglesias@gmail.com>
2021-06-02 11:52:43 +0200

    landing final menu responsive

--- main.js
+++ main.js
@@ -2,4 +2,12 @@ $(document).ready(function(){
     $(".burguer").click(function(){
         $(".lateral").toggleClass("posicion");
     });
+    $(".primero").click(function(){
+        var menu_change = $(this).attr("data-menu"); 
+
+        $(".primero").removeClass("activo");
+        $(this).addClass("activo");
+        $("#"+menu_change).addClass("activo");
+        console.log(menu_change);
+    });
 });
